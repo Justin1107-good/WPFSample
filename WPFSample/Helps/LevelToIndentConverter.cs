6207e56c43e898bfe1803e059fc7e82091769402
王晋 <justin.wang@skycoresaas.com>
2023-11-30 15:25:14 +0800

    树形列表

---
+++ WPFSample/Helps/LevelToIndentConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFSample
+{
+    public class LevelToIndentConverter : IValueConverter
+    {
+        private const double IndentSize = 8.0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Thickness((int)value * IndentSize, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CanExpandConverter : IValueConverter
+    {
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            if ((bool)o)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- WPFSample/MainWindow.xaml.cs
+++ WPFSample/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,68 @@ namespace WPFSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<TestModel> tvData;
         public MainWindow()
         {
             InitializeComponent();
+            ReloadData();
+            this._tv.ItemsSource = tvData;
+        }
+        void ReloadData()
+        {
+            tvData = new ObservableCollection<TestModel>
+            {
+                 new TestModel{ ID=1,Name="张三", tvData =new ObservableCollection<TestModel>{
+                       new TestModel{ ID=2,Name="张李四" },
+                       new TestModel{ ID=3,Name="张王五" },
+                       new TestModel{ ID=4,Name="张马六" },
+                       new TestModel{ ID=5,Name="张张善芬" },
+                 } },
+                  new TestModel{ ID=1,Name="张三", tvData =new ObservableCollection<TestModel>{
+                       new TestModel{ ID=2,Name="张李四" },
+                       new TestModel{ ID=3,Name="张王五" },
+                       new TestModel{ ID=4,Name="张马六" },
+                       new TestModel{ ID=5,Name="张张善芬" },
+                 } },
+            };
+        }
+    }
+    public class TestModel : INotifyPropertyChanged
+    {
+        private int _ID;
+        private string _Name;
+        private ObservableCollection<TestModel> _tvData;
+        public int ID
+        {
+            get { return _ID; }
+            set
+            {
+                _ID = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public ObservableCollection<TestModel> tvData
+        {
+            get { return _tvData; }
+            set
+            {
+                _tvData = value;
+                OnPropertyChanged(nameof(tvData));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
