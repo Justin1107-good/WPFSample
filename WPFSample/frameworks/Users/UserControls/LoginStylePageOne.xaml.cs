ba070440e71bb61b1e44fac1e7ff650131519b4c
王晋 <justin.wang@skycoresaas.com>
2023-12-13 11:56:20 +0800

    添加2个登陆风格用户控件

---
+++ WPFSample/frameworks/Users/UserControls/LoginStylePageOne.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSample.frameworks.Users.UserControls
+{
+    /// <summary>
+    /// LoginStylePageOne.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginStylePageOne : UserControl
+    {
+        public LoginStylePageOne()
+        {
+            InitializeComponent();
+        }
+
+        private void PwdTextBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PwdTextBox.Password == "")
+            {
+                PasswordTip.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PasswordTip.Visibility = Visibility.Hidden;
+            }
+
+        }
+    }
+}
---
+++ WPFSample/frameworks/Users/UserControls/LoginStylePageTwo.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSample.frameworks.Users.UserControls
+{
+    /// <summary>
+    /// LoginStylePageTwo.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginStylePageTwo : UserControl
+    {
+        public LoginStylePageTwo()
+        {
+            InitializeComponent();
+        }
+
+        private void PwdTextBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (PwdTextBox.Password == "")
+            {
+                PasswordTip.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PasswordTip.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
